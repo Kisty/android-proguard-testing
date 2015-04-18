fe8b47cf81b9a2070d485b1f0f56bd28c16a0016
Kay-Uwe Janssen <git@kujanssen.de>
2015-04-18 10:08:16 +0200

    add simple test

--- app/src/androidTest/java/issuerepro/androidproguardtesting/ApplicationTest.java
+++ app/src/androidTest/java/issuerepro/androidproguardtesting/ApplicationTest.java
@@ -2,6 +2,7 @@ package issuerepro.androidproguardtesting;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 
 public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	public ApplicationTest() {
@@ -12,5 +13,11 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
 	public void setUp() throws Exception {
 		super.setUp();
 		getActivity();
+	}
+
+	public void testMenu() throws Exception {
+
+		openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
 	}
 }
